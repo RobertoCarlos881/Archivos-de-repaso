a290d2c1abb7b19d888e444e3a339c0d8c0cf368
RobertoCarlos881 <roberto881carlos881@hotmail.com>
2022-03-10 23:44:51 -0600

    nuevo repaso de javascript

---
+++ JavaScript/Version 2/2-Clase/js/01.js
@@ -0,0 +1,13 @@
+//Variables const 
+const cliente = "Roberto";
+const precioDescuento = 220;
+
+//const no se puede reasignar
+
+console.log(cliente);
+console.log(precioDescuento);
+
+//Reglas de las variables
+//Las varianles no pueden iniciar con numeros o caracteres especiales (Guion bajo si)
+//Las variables no pueden tener espacios
+//Javascript recomienda el uso de camelcase
---
+++ JavaScript/Version 2/3-Clase/js/01.js
@@ -0,0 +1,9 @@
+//Estructuras y tipos de datos
+//Son varios pero algunos de los mas importantes son:
+//Object, null, undefined, number, function, symbol, big int, boolean, string
+
+//Undefined
+let cliente
+
+console.log(cliente);
+console.log(typeof cliente);
