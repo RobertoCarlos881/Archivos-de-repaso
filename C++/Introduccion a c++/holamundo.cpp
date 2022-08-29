cd7f0652cb8a02f297dd85e5f1b5c03c4ac3c6ce
RobertoCarlos881 <roberto881carlos881@hotmail.com>
2022-08-29 18:37:46 -0500

    Subida de c++

---
+++ C++/Introduccion a c++/holamundo.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+//con esta libreria permite tener entrada y salida de datos
+
+using namespace std; //esto sirve para imprimir las cosas en pantalla
+
+int main() {
+    cout<<"Hola mundo \n"<<endl; //endline es una salto de linea
+    return 0;
+}
+
